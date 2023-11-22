8bbfc29ff27100623fc2c0a37ac88aa60c25fef1
bao <bao@bacoor.co>
2023-11-22 13:33:12 +0700

    add more menu items

--- menus/index.tsx
+++ menus/index.tsx
@@ -45,5 +45,62 @@ const menus: menuProps[] = [
       },
     ],
   },
+  {
+    groupName: "NUMBER",
+    items: [
+      {
+        label: "Big Number Calculator",
+        slug: "big-number-calculator",
+        href: "/number/convert-case",
+        icon: null,
+      },
+      {
+        label: "Ethereum Convert Units",
+        slug: "ethereum-convert-units",
+        href: "/number/ethereum-convert-units",
+        icon: null,
+      },
+    ],
+  },
+  {
+    groupName: "DATE",
+    items: [
+      {
+        label: "Epoch Converter",
+        slug: "epoch-converter",
+        href: "/date/epoch-converter",
+        icon: null,
+      },
+      {
+        label: "Date Difference",
+        slug: "date-difference",
+        href: "/date/date-difference",
+        icon: null,
+      },
+    ],
+  },
+  {
+    groupName: "IMAGE",
+    items: [
+      {
+        label: "Image To Base64",
+        slug: "image-to-base64",
+        href: "/image/image-to-base64",
+        icon: null,
+      },
+      {
+        label: "Image Compressor",
+        slug: "image-compressor",
+        href: "/image/image-compressor",
+        icon: null,
+      },
+      {
+        label: "Image Resize",
+        slug: "image-resize",
+        href: "/image/image-resize",
+        icon: null,
+      },
+    ],
+  },
 ];
 export default menus;

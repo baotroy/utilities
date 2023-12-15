cf7bf76fb14fdea87930d86f41b4ac8c8c0b3f62
bao <bao@bacoor.co>
2023-12-15 17:24:26 +0700

    Update Header component styling

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,15 +1,15 @@
-import DarkModeSwitcher from "./DarkModeSwitcher";
-import DropdownMessage from "./DropdownMessage";
-import DropdownNotification from "./DropdownNotification";
-import DropdownUser from "./DropdownUser";
+// import DarkModeSwitcher from "./DarkModeSwitcher";
+// import DropdownMessage from "./DropdownMessage";
+// import DropdownNotification from "./DropdownNotification";
+// import DropdownUser from "./DropdownUser";
 
 const Header = (props: {
   sidebarOpen: string | boolean | undefined;
   setSidebarOpen: (arg0: boolean) => void;
 }) => {
   return (
-    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-      <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+    <header className="sticky top-0 z-999 flex w-full bg-white dark:bg-boxdark">
+      <div className="flex flex-grow items-center justify-between px-4 py-4 md:px-6 2xl:px-11">
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
           <button
@@ -65,22 +65,22 @@ const Header = (props: {
         </div>
 
         <div className="flex items-center gap-3 2xsm:gap-7">
-          <ul className="flex items-center gap-2 2xsm:gap-4">
-            {/* <!-- Dark Mode Toggler --> */}
-            <DarkModeSwitcher />
-            {/* <!-- Dark Mode Toggler --> */}
+          {/* <ul className="flex items-center gap-2 2xsm:gap-4"> */}
+          {/* <!-- Dark Mode Toggler --> */}
+          {/* <DarkModeSwitcher /> */}
+          {/* <!-- Dark Mode Toggler --> */}
 
-            {/* <!-- Notification Menu Area --> */}
-            <DropdownNotification />
-            {/* <!-- Notification Menu Area --> */}
+          {/* <!-- Notification Menu Area --> */}
+          {/* <DropdownNotification /> */}
+          {/* <!-- Notification Menu Area --> */}
 
-            {/* <!-- Chat Notification Area --> */}
-            <DropdownMessage />
-            {/* <!-- Chat Notification Area --> */}
-          </ul>
+          {/* <!-- Chat Notification Area --> */}
+          {/* <DropdownMessage /> */}
+          {/* <!-- Chat Notification Area --> */}
+          {/* </ul> */}
 
           {/* <!-- User Area --> */}
-          <DropdownUser />
+          {/* <DropdownUser /> */}
           {/* <!-- User Area --> */}
         </div>
       </div>

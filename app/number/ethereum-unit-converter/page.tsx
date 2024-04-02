11f2ddf2f71039d8ccbbc59992424afd2a14b148
bao <bao@bacoor.co>
2024-04-02 17:35:42 +0700

    add jwt decoder title

--- app/number/ethereum-unit-converter/page.tsx
+++ app/number/ethereum-unit-converter/page.tsx
@@ -1,11 +1,11 @@
+import { Metadata } from "next";
+import EtherUnitConverterComponent from "../components/ethereum-unit-converter";
 export const metadata: Metadata = {
   title: "Ether Unit Converter",
   description: "Unit Conversion in the Ethereum Network.",
   // other metadata
 };
 
-import { Metadata } from "next";
-import EtherUnitConverterComponent from "../components/ethereum-unit-converter";
 const EtherUnitConverter = () => {
   return <EtherUnitConverterComponent />;
 };
